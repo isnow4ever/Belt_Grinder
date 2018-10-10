926c4c45aea952b8f9c7b718ea6d3c0680a16634
isnow4ever <gaoyongzhuo@yeah.net>
2018-10-10 14:00:02 +0800

    program structure

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/workspace/Belt_Grinder/src
-# Build directory: /root/workspace/Belt_Grinder/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
